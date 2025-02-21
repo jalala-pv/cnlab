dba9dab8ee62592d7ac688cdc8342bc10e77f704
jalala-pv <99414691+jalala-pv@users.noreply.github.com>
2025-02-21 11:01:45 +0530

    Add files via upload

---
+++ week1/sum.sh
@@ -0,0 +1,10 @@
+
+#!/bin/bash
+echo "enter the value a"
+read a
+echo "enter the value b"
+read b
+sum=`expr $a + $b `
+echo "the sum of two values are $sum"
+
+
---
+++ week1/var.sh
@@ -0,0 +1,10 @@
+
+#!/bin/bash
+var=2
+echo "$var"
+((sum=20 + 30))
+echo $sum
+echo "what is your name"
+read name
+echo "my name is $name"
+
